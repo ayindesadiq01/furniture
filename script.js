0939949dd1f5097cc29d1fd5ea57526f93655981
ayindesadiq01 <ayindesadiq01@gmail.com>
2024-01-20 19:23:52 +0100

    progress

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+const progressEl = document.querySelector('.progress');
+const html = document.querySelector('html');
+
+
+// WORKINGK WITH PROGRESS BAR
+window.addEventListener('scroll', () => {
+  const srcHeight = window.innerHeight;
+  const scrollY = Math.round(window.scrollY);
+  const scrollHeight = html.scrollHeight;
+
+  const scrollCalc = srcHeight + scrollY;
+  const progress = scrollCalc / scrollHeight * 100;
+
+  progressEl.value = progress;
+});
+
+// WORKING WITH SCROLL BAR
+const homePage = document.querySelector('.homepage');
+const home = document.querySelector('.home');
+
+const conObFtn = function(entries) {
+  const [entry] = entries;
+    // console.log(entry)
+
+  if(!entry.isIntersecting) {
+    homePage.classList.add('sticky-container')
+  } else {
+    homePage.classList.remove('sticky-container')
+  }
+}
+
+const contOb = new IntersectionObserver(conObFtn, {
+  root: null,
+  threshold: 0
+})
+
+contOb.observe(home);
