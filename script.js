454acb45ef4175eb26f27b38c5f7fd1454de9acd
ayindesadiq01 <ayindesadiq01@gmail.com>
2024-01-27 14:55:10 +0100

    hide location on scroll

--- script.js
+++ script.js
@@ -41,7 +41,8 @@ const hidden = document.querySelector('.hidden');
 const menuBar = document.querySelector('.menu-bar');
 const menuIcon = document.querySelector('.menu-icon');
 const closeIcon = document.querySelector('.close-icon');
-const blurr = document.querySelector('.blurry')
+const blurr = document.querySelector('.blurry');
+
 
 menuIcon.addEventListener('click', () => {
   menuBar.classList.remove('hidden');
@@ -55,15 +56,16 @@ closeIcon.addEventListener('click', () => {
 
 // REVEALING ON SCROLL
 const allSection = document.querySelectorAll('.section');
+const locationInfo = document.querySelector('.location-info');
 
 const revSection = function(entries, observer) {
 
   const [entry] = entries;
-  // console.log(entry)
 
   if(entry.isIntersecting) {
     entry.target.classList.remove('section-hidden');
   }
+
 }
 
 const intSection = new IntersectionObserver(revSection, {
@@ -75,3 +77,35 @@ allSection.forEach(section => {
   intSection.observe(section);
   section.classList.add('section-hidden')
 });
+
+
+// HIDING THE LOCATION INFO
+const homee = document.querySelector('.home');
+const bod = document.querySelector('body');
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+
+const homSection = function(entries, observer) {
+  const [entry] = entries;
+  console.log(entry)
+
+  if(!entry.isIntersecting) {
+
+      if(mediaQuery.matches == true) {
+        console.log(mediaQuery)
+        locationInfo.style.display = 'none'
+      } 
+    } else if(entry.isIntersecting) {
+      if(mediaQuery.matches == true) {
+        console.log(mediaQuery)
+        locationInfo.style.display = 'flex'
+      } 
+    }
+}
+
+const homeSection = new IntersectionObserver(homSection, {
+  root: null,
+  threshold: 0,
+});
+
+homeSection.observe(homee)
